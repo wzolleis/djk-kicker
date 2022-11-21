8e29652e41f02f05cac44682d9ebc9d05e0453ed
Jack <wzolleis@web.de>
2022-11-21 01:51:06 +0100

    Einladungslink verschicken

--- app/components/i18n/messages.ts
+++ app/components/i18n/messages.ts
@@ -85,20 +85,37 @@ const messages = {
     warnings: {
         noToken: "Du bist nicht berechtigt einen Status abzusenden oder einen neuen Spieler anzulegen, da du diese Seite ohne einen gültigen Einladungslink besucht hast. Rufe die Seite entweder über einen gültigen Einladungslink auf oder wende dich an einen Administrator."
     },
+    mailContent: {
+        invitationMail: {
+            mailSubject: (datum: string) => `Einladung für das DJK-Kicker Fussballspiel am ${datum}`,
+            mailBody: ({datum, spielOrt, einladungsLink, playerName}: {datum: string, spielOrt: string, einladungsLink: string, playerName: string}) => `Hallo ${playerName}
+<br/>
+Einladung für das Spiel am ${datum}.
+<br/>
+Spielort: ${spielOrt} 
+<br/>
+Bitte klicke auf die <a href=${einladungsLink}>Einladung</a> und teile uns mit, ob Du kommen kannst.
+<br/>
+Verwende den Link bitte auch, wenn Du Deine Entscheidung nochmal ändern möchtest.
+<br/>
+<br/>
+Vielen Dank 
+`
+        },
+    },
     adminGameInvitationForm: {
         titleGame: "Einadung für das Spiel am: ",
         titleGameTime: "Spielort: ",
         mailBodyLabel: "Nachricht",
         mailReceiver: "An:",
+        sendInvitationBtn: 'Einladung verschicken',
         spielort: (optionValue: string) => `${Number.parseInt(optionValue) === spielortOptions.halle.value ? 'in der Halle' : 'draußen'}`,
         mailSubjectLabel: "Betreff",
-        mailSubject: (datum: string) => `Einladung für das DJK-Kicker Fussballspiel am ${datum}`,
-        mailBody: (datum: string, spielOrt: string) => `Hallo <Name>
-
+        mailBody: (datum: string) => `Hallo Spieler
 Einladung für das Spiel am ${datum}.
-Spielort: ${spielOrt}
+Spielort: Halle/Draußen 
 
-Bitte klicke auf den <Einladungslink> und teile uns mit, ob Du kommen kannst.
+Bitte klicke auf die Einladung und teile uns mit, ob Du kommen kannst.
 Verwende den Link bitte auch, wenn Du Deine Entscheidung nochmal ändern möchtest.
 
 Vielen Dank 
