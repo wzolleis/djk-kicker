2e04d44a1690f51037b0d3b9e52f27c311550e78
Jack <wzolleis@web.de>
2023-08-17 01:00:09 +0200

    DJK-114: Matches sortieren, aktuell nur Ausgabe auf der Konsole

--- app/components/ratings/MatchList.tsx
+++ app/components/ratings/MatchList.tsx
@@ -2,6 +2,7 @@ import {ItemSelection} from "~/components/selection";
 import {RatingWithId} from "~/models/classes/RatingWithId";
 import {calculateMatches} from "~/matches/teamCalculation";
 import {mapToPlayerModel} from "~/matches/player";
+import {MatchModel} from "~/matches/teamTypes";
 
 export type MatchListProps = {
     selection: ItemSelection<RatingWithId>
@@ -11,7 +12,11 @@ const MatchList = ({selection}: MatchListProps) => {
     const players = mapToPlayerModel(selection.includedItems)
     const matches = calculateMatches(players)
 
-    console.log('matches', matches)
+    const sortBySkill = (m1: MatchModel, m2: MatchModel) => {
+        return m1.diff - m2.diff
+    }
+
+    console.log('matches', matches.sort(sortBySkill))
 
     return (
         <div>Matches</div>
